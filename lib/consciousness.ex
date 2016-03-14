7e8b66dc0adfa1ebbb87c5fbbfadb5c81a136c03
Arthur Colle <hello@arthurcolle.com>
2016-03-13 22:11:14 -0400

    123

---
+++ lib/consciousness.ex
@@ -0,0 +1,2 @@
+defmodule Consciousness do
+end
---
+++ test/consciousness_test.exs
@@ -0,0 +1,8 @@
+defmodule ConsciousnessTest do
+  use ExUnit.Case
+  doctest Consciousness
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
